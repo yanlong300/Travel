ed045417b16ab42fd02382d6c22fc9bb10578ad8
闫龙 <yan.long@ielpm.com>
2018-05-30 13:35:44 +0800

    change

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="header-input">
       <span class="iconfont">&#xe632;</span>
-      输入城市/景点/游玩主题
+      输入城市/景点/游玩主题/地区
     </div>
     <div class="header-right">
       城市
